fb4d9d7836b79dea3c3688c521215d7b75f356c6
MINSK\dzya <yalovik.dima@gmail.com>
2019-02-27 20:28:56 +0300

    Dz3 Loading images using lib picasso

--- app/src/main/java/com/gmail/yalovikdima/itacademy/dz3/Dz3Activity.java
+++ app/src/main/java/com/gmail/yalovikdima/itacademy/dz3/Dz3Activity.java
@@ -2,15 +2,33 @@ package com.gmail.yalovikdima.itacademy.dz3;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.gmail.yalovikdima.itacademy.R;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
-public class Dz3Activity extends AppCompatActivity {
+public class Dz3Activity extends AppCompatActivity implements View.OnClickListener {
 
-    public static Intent getIntent(Context context){
+    ImageView imageView;
+    Button buttonLoad;
+    EditText link;
+    ProgressBar progressBar;
+
+    public static Intent getIntent(Context context) {
         return new Intent(context, Dz3Activity.class);
     }
 
@@ -18,5 +36,58 @@ public class Dz3Activity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dz3);
+        init();
+    }
+
+    private void init(){
+        imageView = findViewById(R.id.imageView);
+        buttonLoad = findViewById(R.id.buttonLoad);
+        link = findViewById(R.id.link);
+        progressBar = findViewById(R.id.progressBar);
+        buttonLoad.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        progressBar.setVisibility(View.VISIBLE);
+        Picasso.get().load(link.getText().toString()).transform(new CircularTransformation()).into(imageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                progressBar.setVisibility(View.INVISIBLE);
+                imageView.setImageResource(R.drawable.ic_error_black);
+            }
+        });
+    }
+
+    public class CircularTransformation implements Transformation {
+
+        public CircularTransformation() {
+        }
+
+        @Override
+        public Bitmap transform(final Bitmap source) {
+            final Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setShader(new BitmapShader(source, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+
+            final Bitmap output = Bitmap.createBitmap(source.getWidth(), source.getHeight(), Bitmap.Config.ARGB_8888);
+            final Canvas canvas = new Canvas(output);
+            canvas.drawCircle(source.getWidth() / 2, source.getHeight() / 2, Math.min(source.getWidth() / 2, source.getHeight() / 2), paint);
+
+            if (source != output)
+                source.recycle();
+
+            return output;
+        }
+
+        @Override
+        public String key() {
+            return "circle";
+        }
     }
 }
