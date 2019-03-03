0ef71fe76a6ab155191c5a7e14e6ed81169d20d5
yalovik.dima@gmail.com <yalovik.dima@gmail.com>
2019-03-04 00:05:52 +0300

    Dz4.Add Clock view

---
+++ app/src/main/java/com/gmail/yalovikdima/itacademy/views/ClockView.java
@@ -0,0 +1,122 @@
+package com.gmail.yalovikdima.itacademy.views;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
+import java.util.Calendar;
+
+public class ClockView extends View {
+
+    private float height;
+    private float width;
+    private float padding;
+    private int fontSize;
+    private float handTruncation;
+    private float hourHandTruncation;
+    private float radius;
+    private boolean isInit;
+    private int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    private Rect rect;
+    private Paint paint;
+
+
+    public ClockView(Context context) {
+        super(context);
+    }
+
+    public ClockView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ClockView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @SuppressLint("NewApi")
+    public ClockView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void initClock() {
+        height = getHeight();
+        width = getWidth();
+        padding = 50;
+        fontSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 22, getResources().getDisplayMetrics());
+        float min = Math.min(height, width);
+        radius = min / 2 - padding;
+        handTruncation = min / 20;
+        hourHandTruncation = min / 7;
+        paint = new Paint();
+        isInit = true;
+        rect = new Rect();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (!isInit) {
+            initClock();
+        }
+        canvas.drawColor(Color.BLACK);
+        drawCircle(canvas);
+        drawCenter(canvas);
+        drawNumeral(canvas);
+        drawHands(canvas);
+
+        postInvalidateDelayed(500);
+        invalidate();
+    }
+
+    private void drawHand(Canvas canvas, double loc, boolean isHour) {
+        double angle = Math.PI * loc / 30 - Math.PI / 2;
+        int handRadius = (int) (isHour ? radius - handTruncation - hourHandTruncation : radius - handTruncation);
+        canvas.drawLine(width / 2, height / 2,
+                (float) (width / 2 + Math.cos(angle) * handRadius),
+                (float) (height / 2 + Math.sin(angle) * handRadius), paint);
+    }
+
+    private void drawHands(Canvas canvas) {
+        Calendar c = Calendar.getInstance();
+        float hour = c.get(Calendar.HOUR_OF_DAY);
+        hour = hour > 12 ? hour -12 : hour;
+        drawHand(canvas, (hour + c.get(Calendar.MINUTE) / 60) * 5f, true);
+        drawHand(canvas, c.get(Calendar.MINUTE), false);
+        drawHand(canvas, c.get(Calendar.SECOND), false);
+
+    }
+
+    private void drawNumeral(Canvas canvas) {
+        paint.setTextSize(fontSize);
+
+        for (int number : numbers) {
+            String tmp = String.valueOf(number);
+            paint.getTextBounds(tmp, 0, tmp.length(), rect);
+            double angle = Math.PI / 6 * (number-3);
+            float x = (float) (width / 2 + Math.cos(angle) * radius - rect.width() / 2);
+            float y = (float) (height / 2 + Math.sin(angle) * radius + rect.height() / 2);
+            canvas.drawText(tmp, x, y, paint);
+        }
+    }
+
+    private void drawCenter(Canvas canvas) {
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(width / 2, height / 2, 18, paint);
+    }
+
+    private void drawCircle(Canvas canvas) {
+        paint.reset();
+        paint.setColor(Color.WHITE);
+        paint.setStrokeWidth(10);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+        canvas.drawCircle(width / 2, height / 2, radius + padding - 10, paint);
+    }
+}
