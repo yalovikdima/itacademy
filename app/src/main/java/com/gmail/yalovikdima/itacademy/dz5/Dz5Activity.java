357321472fa8e0199d03408246fe2cc2a848484c
MINSK\dzya <yalovik.dima@gmail.com>
2019-03-12 11:47:23 +0300

    Dz5. Complete

--- app/src/main/java/com/gmail/yalovikdima/itacademy/dz5/Dz5Activity.java
+++ app/src/main/java/com/gmail/yalovikdima/itacademy/dz5/Dz5Activity.java
@@ -1,23 +1,30 @@
 package com.gmail.yalovikdima.itacademy.dz5;
 
-import android.app.IntentService;
+import android.app.Activity;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.Switch;
 
 import com.gmail.yalovikdima.itacademy.R;
-import com.gmail.yalovikdima.itacademy.services.WifiService;
-import com.gmail.yalovikdima.itacademy.util.WifiBroadcastReceiver;
 
-public class Dz5Activity extends AppCompatActivity {
+import static com.gmail.yalovikdima.itacademy.dz5.WifiService.*;
+import static com.gmail.yalovikdima.itacademy.dz5.WifiService.CHECK_WIFI;
+import static com.gmail.yalovikdima.itacademy.dz5.WifiService.STATE;
 
-    private TextView textView;
-    WifiBroadcastReceiver mMessageReceiver = new WifiBroadcastReceiver();
+public class Dz5Activity extends Activity {
+
+    private Switch switchWifi;
+    private WifiService wifiService = new WifiService();
+    private ServiceConnection serviceConnection;
 
     public static Intent getIntent(Context context) {
         return new Intent(context, Dz5Activity.class);
@@ -27,32 +34,53 @@ public class Dz5Activity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dz5);
-        textView = findViewById(R.id.textView);
+        switchWifi = findViewById(R.id.wifi_switch);
 
+        serviceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                wifiService = ((WifiService.MyBinder) iBinder).getService();
+                wifiService.checkWifiState();
+            }
 
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+            }
+        };
     }
 
     @Override
     protected void onResume() {
-        startService(new Intent(Dz5Activity.this,
-                WifiService.class));
-
-        IntentFilter intentFilter = new IntentFilter("custom-event-name");
-        LocalBroadcastManager.getInstance(this).registerReceiver(
-                mMessageReceiver, intentFilter);
-
         super.onResume();
+        Intent intent = new Intent(this, WifiService.class);
+        bindService(intent, serviceConnection, Service.BIND_AUTO_CREATE);
+
+        IntentFilter intentFilter = new IntentFilter(CHECK_WIFI);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, intentFilter);
     }
 
     @Override
     protected void onPause() {
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(
-                mMessageReceiver);
-
-        stopService(new Intent(Dz5Activity.this,
-                WifiService.class));
-
         super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
+        unbindService(serviceConnection);
     }
+
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent != null) {
+                if (intent.getBooleanExtra(STATE, false)) {
+                    switchWifi.setChecked(true);
+                    switchWifi.setText("WiFi is ON");
+                } else {
+                    switchWifi.setChecked(false);
+                    switchWifi.setText("WiFi is OFF");
+                }
+
+            }
+        }
+    };
 
 }
